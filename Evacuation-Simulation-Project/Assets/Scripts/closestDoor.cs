340b4589639dc9dd0e8742f401e5341b412e0d9a
danicretu <cretu_daniela92@yahoo.com>
2014-02-22 02:20:03 +0000

    added pathfinding

---
+++ Evacuation-Simulation-Project/Assets/Scripts/closestDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+using RAIN.Action;
+public class closestDoor : MonoBehaviour {
+	public Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+
+		GameObject[] doors = GameObject.FindGameObjectsWithTag("Door");
+		float minDistance = Vector3.Distance(transform.position, doors[0].transform.position);
+		string doorName=doors[0].name;
+		GameObject door=doors[0];
+		int i=1;
+		while (i<doors.Length){
+			float distance=Vector3.Distance(transform.position, doors[i].transform.position);
+			if (distance < minDistance){
+				doorName=doors[i].name;
+				minDistance=distance;
+				door=doors[i];
+			}
+			i++;
+		}
+		RAINAgent ai=GetComponent<RAINAgent>();
+		if (ai!=null){
+			//print ("not null");
+			ai.Agent.actionContext.SetContextItem<string>("door", doorName);
+		}
+		else print ("null");
+		targetPosition=door.transform.position;
+		//print(ai.Agent.actionContext.GetContextItem<string>("door"));
+		
+	}
+	
+
+}
---
+++ Evacuation-Simulation-Project/Assets/Scripts/way.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+using RAIN.Path;
+using RAIN.Action;
+
+public class way : Action {
+	private RAIN.Path.RAINPathManager path;
+
+	public override ActionResult Start (Agent agent, float deltaTime) {
+		
+		//way(
+		GameObject wpc = GameObject.Find("Waypoints1");
+		
+		if (wpc != null )
+		{
+			path= (RAIN.Path.RAINPathManager)  agent.PathManager;
+			
+			if (path != null )
+			{
+				Debug.Log("got here");
+				path.waypointCollection = wpc;
+				path.ReInit();
+			}
+			else Debug.Log("nope");
+		}
+		return ActionResult.SUCCESS;
+	}
+}
