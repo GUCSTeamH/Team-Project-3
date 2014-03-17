a0dc2aafcac1f8a39d316c76fc63d4063d99d32d
Andrew Tully <andrew_tully@hotmail.co.uk>
2014-03-17 13:55:27 +0000

    added behaviour types to passenger prefab

---
+++ Evacuation-Simulation-Project/Assets/Scripts/Attribute.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+using RAIN.Action;
+
+public class Attribute : MonoBehaviour {
+
+	public int monteCarlo(int min, int max){
+		return (Random.Range(min, max));
+	}
+	
+	/* Types - Altruism, Behavioural-Inaction, Panic, Fear-Flight */ 
+	public void setType(){
+		RAINAgent ai = GetComponent<RAINAgent>();
+		if (ai!=null){
+			int random = monteCarlo(0, 100);
+			
+			if (random < 25){
+				ai.Agent.actionContext.SetContextItem<string>("type", "altruism");
+			}
+			else if (random < 50){
+				ai.Agent.actionContext.SetContextItem<string>("type", "behaviouralinaction");
+			}
+			else if (random < 75){
+				ai.Agent.actionContext.SetContextItem<string>("type", "fearflight");
+			}
+			else{
+				ai.Agent.actionContext.SetContextItem<string>("type", "panic");
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		setType();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
