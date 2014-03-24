0bf607c00d45722c18507ca778d73e024bbb1dee
IoannisCleary <nicolemaniac@gmail.com>
2014-03-24 14:46:00 +0000

    Main menu design update

---
+++ Evacuation-Simulation-Project/Assets/Scripts/move_Straight.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+var mDirection:Vector3;
+var controller : CharacterController;
+ 
+function Update() 
+{
+    //Direction.z-=2*Time.deltaTime;
+    mDirection.y+=3*Time.deltaTime;
+    mDirection.x-=5*Time.deltaTime;
+    controller.Move(mDirection * Time.deltaTime);
+}
