490c4ca9061df94da7c119b512eb1c2db391e6c6
danicretu <cretu_daniela92@yahoo.com>
2014-03-24 14:54:41 +0000

    fixed bug with back doors

--- Evacuation-Simulation-Project/Assets/Scripts/closestDoor.cs
+++ Evacuation-Simulation-Project/Assets/Scripts/closestDoor.cs
@@ -18,11 +18,12 @@ public class closestDoor : MonoBehaviour {
 		GameObject plane = GameObject.FindGameObjectWithTag("plane");
 		doorScript =(getDoors) plane.GetComponent("getDoors");
 		doorslist = doorScript.doorslist;
-	//	Debug.Log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+doorslist.Count);
+		Debug.Log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+doorslist.Count);
 
 		float minDistance = Vector3.Distance(transform.position, doorslist[0].transform.position);
 		string doorName=doorslist[0].name;
 		targetPosition = doorslist[0].transform.position;
+
 
 		string doorName2 = null;
 		Vector3 targetPosition2 = new Vector3(0,0,0);
@@ -56,7 +57,7 @@ public class closestDoor : MonoBehaviour {
 				ai.Agent.actionContext.SetContextItem<Vector3>("door", targetPosition);
 				ai.Agent.actionContext.SetContextItem<string>("doorName", doorName);
 			}
-			Debug.Log(doorName+"****************************************"+doorName2);
+			Debug.Log("****************************************"+ai.Agent.actionContext.GetContextItem<string>("doorName"));
 			string type = ai.Agent.actionContext.GetContextItem<string>("type");
 			
 			if (type == "altruism"){
