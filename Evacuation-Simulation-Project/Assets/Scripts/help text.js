8488b6148e7eb5296edb8d3899ef63ac196e43c0
danicretu <cretu_daniela92@yahoo.com>
2014-03-26 16:01:43 +0000

    deleted redundant things

--- Evacuation-Simulation-Project/Assets/Scripts/help text.js
+++
@@ -1,9 +0,0 @@
-﻿    var veryLongString = "insert very long text here";
-    var scrollPosition : Vector2 = Vector2.zero;
-     
-     
-   function Start(){  
-    scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.MaxHeight(300), GUILayout.ExpandHeight (false));
-    GUILayout.Label (veryLongString, GUILayout.ExpandHeight (true));
-    GUILayout.EndScrollView();
-   }
